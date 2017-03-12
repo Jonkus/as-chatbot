f938d9de132d8d6e60bfa0c8dce294116dd163ee
Jonathan Kuhse <jonathan-github@kuhse.xyz>
2017-03-12 15:05:45 +0100

    true d6

--- diceroll.go
+++ diceroll.go
@@ -7,8 +7,8 @@ import (
 
 func diceroll() (string, error) {
 
-	d1 := rand.Intn(5) + 1
-	d2 := rand.Intn(5) + 1
+	d1 := rand.Intn(6) + 1
+	d2 := rand.Intn(6) + 1
 
 	return fmt.Sprintf("(%d + %d) = %d", d1, d2, d1+d2), nil
 }
